f2d245e08579475fd3e45db9c35c8667500a7f9f
chandralaxmi <chandra.laxmi@flipkart.com>
2015-07-27 16:27:32 +0530

    Update board.rb

--- board.rb
+++ board.rb
@@ -1,3 +1,6 @@
+#Board contains 9 cells of the tictactoe game. It has function named printer which prints the current status of board, checkforwin which checks whether some player had won or not, and checkfordraw which checks whether board is in current state of draw or not
+
+
 require_relative 'cell'
 class Board
 	
@@ -5,82 +8,80 @@ class Board
 		@b=[]
 		0.upto(8) {@b.push(Cell.new)}
     end
-    def b
-    	@b
-    end
+    attr_reader :b
     def printer
     	#puts ""
     	system("clear")
     	0.upto(2){ |x| 
-    	case
-    	when @b[x].occupiedBy==1
-    		print "x "
-    	when @b[x].occupiedBy==2
-    		print "y "
-    	else
-    		print ". "
-    	end
-    	}
-    	puts ""
-    	3.upto(5){ |x| 
-    	case
-    	when @b[x].occupiedBy==1
-    		print "x "
-    	when @b[x].occupiedBy==2
-    		print "y "
-    	else
-    		print ". "
-    	end
-    	}
-    	puts "" 
-    	6.upto(8){ |x| 
-    	case
-    	when @b[x].occupiedBy==1
-    		print "x "
-    	when @b[x].occupiedBy==2
-    		print "y "
-    	else
-    		print ". "
-    	end
-    	}
-    	puts ""	
-    end
-    def checkforwin(var)
-    		case
-            when @b[0].occupiedBy==var && @b[1].occupiedBy==var && @b[2].occupiedBy==var
-		    		true
-			when @b[3].occupiedBy==var && @b[4].occupiedBy==var && @b[5].occupiedBy==var
-					true
-	        when @b[6].occupiedBy==var && @b[7].occupiedBy==var && @b[8].occupiedBy==var
-		       	true
-	        when @b[0].occupiedBy==var && @b[3].occupiedBy==var && @b[6].occupiedBy==var
-		         true
-            when @b[1].occupiedBy==var && @b[4].occupiedBy==var && @b[7].occupiedBy==var
-            	true
-            when @b[2].occupiedBy==var && @b[5].occupiedBy==var && @b[8].occupiedBy==var
-            	true
-            when @b[0].occupiedBy==var && @b[4].occupiedBy==var && @b[8].occupiedBy==var
-            	true
-            when @b[2].occupiedBy==var && @b[4].occupiedBy==var && @b[6].occupiedBy==var
-            	true
-            else
-            	false
-            end
-    	end	
-    def checkfordraw
-    	flag=0
-    	0.upto(8){|x| 
-    		if @b[x].occupiedBy==0 
-               flag=1
-               break
-            end
-        }
-    	case 
-    		when flag==1
-    			false
-    		else
-    			true
-    		end	
-    	end
-    	
+           case
+           when @b[x].occupiedBy==1
+              print "x "
+          when @b[x].occupiedBy==2
+              print "y "
+          else
+              print ". "
+          end
+      }
+      puts ""
+      3.upto(5){ |x| 
+       case
+       when @b[x].occupiedBy==1
+          print "x "
+      when @b[x].occupiedBy==2
+          print "y "
+      else
+          print ". "
+      end
+  }
+  puts "" 
+  6.upto(8){ |x| 
+   case
+   when @b[x].occupiedBy==1
+      print "x "
+  when @b[x].occupiedBy==2
+      print "y "
+  else
+      print ". "
+  end
+}
+puts ""	
+end
+def checkforwin(var)
+  case
+  when @b[0].occupiedBy==var && @b[1].occupiedBy==var && @b[2].occupiedBy==var
+    true
+when @b[3].occupiedBy==var && @b[4].occupiedBy==var && @b[5].occupiedBy==var
+   true
+when @b[6].occupiedBy==var && @b[7].occupiedBy==var && @b[8].occupiedBy==var
+  true
+when @b[0].occupiedBy==var && @b[3].occupiedBy==var && @b[6].occupiedBy==var
+ true
+when @b[1].occupiedBy==var && @b[4].occupiedBy==var && @b[7].occupiedBy==var
+   true
+when @b[2].occupiedBy==var && @b[5].occupiedBy==var && @b[8].occupiedBy==var
+   true
+when @b[0].occupiedBy==var && @b[4].occupiedBy==var && @b[8].occupiedBy==var
+   true
+when @b[2].occupiedBy==var && @b[4].occupiedBy==var && @b[6].occupiedBy==var
+   true
+else
+   false
+end
+end	
+def checkfordraw
+   flag=0
+   0.upto(8){|x| 
+      if @b[x].occupiedBy==0 
+         flag=1
+         break
+     end
+ }
+ case 
+ when flag==1
+     false
+ else
+     true
+ end	
+end
+
 end
